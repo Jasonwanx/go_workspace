59581d1c8eb0fe06cbc7d69199e725e587c89cf3
Jasonwanx <wx313421843@163.com>
2018-11-06 20:45:55 +0800

    restapi实现

--- src/networkProject/src/cacheTest/cacheTest.go
+++ src/networkProject/src/cacheTest/cacheTest.go
@@ -2,26 +2,38 @@ package main
 
 import (
 	"fmt"
-	"github.com/go-ini/ini"
 	"github.com/patrickmn/go-cache"
-	"log"
 	"time"
 )
 
+func callBack(key string, value interface{}){
+	fmt.Println("callBack:", key)
+}
+
 func main(){
 	c := cache.New(5*time.Minute, 10*time.Minute)
+
+	c.OnEvicted(callBack)
+
 	c.Set("foo", "bar22222222", cache.DefaultExpiration)
+	c.Set("ip", "200.10.26.200", cache.DefaultExpiration)
+
+	c.SaveFile("data.txt")
 
 	foo, found := c.Get("foo")
+	ip, found := c.Get("ip")
 	if found {
 		fmt.Println(foo)
+		fmt.Println(ip)
 	}
 
-	cfg,err := ini.Load("TsiDemoARConfig.ini")
-	if err != nil{
-		log.Fatal(err)
-	}
+	c.Delete("foo")
 
-	OrderFileDir := cfg.Section("SearchDir").Key("OrderFileDir")
-	fmt.Println(OrderFileDir)
+	//cfg,err := ini.Load("TsiDemoARConfig.ini")
+	//if err != nil{
+	//	log.Fatal(err)
+	//}
+	//
+	//OrderFileDir := cfg.Section("SearchDir").Key("OrderFileDir")
+	//fmt.Println(OrderFileDir)
 }
